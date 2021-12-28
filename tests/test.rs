3f51148dafc7bfeb70733deee25c4632beaae913
Antonio Caggiano <info@antoniocaggiano.eu>
2021-12-28 19:55:50 +0100

    pw: Procedural macro
    We can analyze rust shaders using a procedural macro. This macro takes a
    shader path as input and Vulkan code should be generated automatically
    for us behind the scenes.

--- tests/test.rs
+++ tests/test.rs
@@ -2,7 +2,11 @@
 // Author: Antonio Caggiano <info@antoniocaggiano.eu>
 // SPDX-License-Identifier: MIT
 
+use pipewriter::*;
+
+pipewriter_macro!("tests/shader/simple/src/simple.rs");
+
 #[test]
 fn build_simple_shader() {
-    assert!(pipewriter::run() == 1);
+    assert!(1 == 1);
 }
