f58a6a7c83d86c2b32843ee11dcf88eddc8af734
Antonio Caggiano <info@antoniocaggiano.eu>
2021-12-29 07:31:57 +0100

    pw: Build shader
    Add spirv-builder as a dependency of pipetest and use it to build the
    simple shader.

--- pipetest/tests/test.rs
+++ pipetest/tests/test.rs
@@ -7,6 +7,12 @@ use pipewriter::*;
 pipewriter_macro!("pipetest/shader/simple/src/simple.rs");
 
 #[test]
+fn load_simple_shader() {
+    const SHADERS: &[u8] = include_bytes!(env!("simple_shader.spv"));
+    assert!(!SHADERS.is_empty());
+}
+
+#[test]
 fn build_simple_shader() {
     let _main_pipeline = PipelineMain {};
     let _secondary_pipeline = PipelineSecondary {};
